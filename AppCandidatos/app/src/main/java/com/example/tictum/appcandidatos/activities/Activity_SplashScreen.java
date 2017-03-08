e3c4fa749339a8a4f16d075d55c34f6d1de32b67
Martinez17 <bryanmartinezh17@gmail.com>
2017-03-08 11:21:46 +0100

    nombres layouts modificados para que no salgan errores

--- AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/activities/Activity_SplashScreen.java
+++ AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/activities/Activity_SplashScreen.java
@@ -10,6 +10,6 @@ public class Activity_SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout__splah_screen);
+        setContentView(R.layout.layout__splash_screen);
     }
 }
--- AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/adapter/AdaptadorEntrevistas.java
+++ AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/adapter/AdaptadorEntrevistas.java
@@ -1,13 +1,39 @@
 package com.example.tictum.appcandidatos.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.example.tictum.appcandidatos.R;
 import com.example.tictum.appcandidatos.beans.Entrevista;
 
 public class AdaptadorEntrevistas extends ArrayAdapter<Entrevista> {
 
-    public AdaptadorEntrevistas(Context context,int resource) {
-        super(context, resource);
+    class ViewHolder{
+    TextView nombreEntrevista;
     }
+    // constructor del adaptador para mostrar la lista de entrevistas
+    public AdaptadorEntrevistas(Context context,Entrevista[] listaEntrevistas) {
+        super(context, R.layout.layout_mostrar_entrevistas,listaEntrevistas);
+    }
+
+    // obtenemos la vista de cada objeto entrevista y lo mostramos
+    public View getView(int posicion,View convertView, ViewGroup parent){
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View itemView = inflater.inflate(R.layout.layout_mostrar_entrevistas,null);
+        AdaptadorEntrevistas.ViewHolder holder;
+        holder = new AdaptadorEntrevistas.ViewHolder();
+
+        holder.nombreEntrevista = (TextView) itemView.findViewById(R.id.nombreEntrevista);
+
+        Entrevista entrevista = getItem(posicion);
+
+        holder.nombreEntrevista.setText(entrevista.getNombrePuesto());
+
+        return (itemView);
+    }
+
 }
