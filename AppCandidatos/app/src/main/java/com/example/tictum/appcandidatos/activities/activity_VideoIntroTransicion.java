2488197b8ee343bee93848f25b83f4b7f02d7295
Martinez17 <bryanmartinezh17@gmail.com>
2017-03-08 11:54:23 +0100

    noactionbar

--- AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/activities/activity_VideoIntroTransicion.java
+++ AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/activities/activity_VideoIntroTransicion.java
@@ -1,15 +1,35 @@
 package com.example.tictum.appcandidatos.activities;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
 
 import com.example.tictum.appcandidatos.R;
 
-public class activity_VideoIntroTransicion extends AppCompatActivity {
+public class activity_VideoIntroTransicion extends AppCompatActivity{
+
+        private VideoView videoView;
+        private int position = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.layout__video_intro_transicion);
+
+        // Muestra el Activity en modo Portrait.
+        this.setRequestedOrientation(
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        setUpVideoView();
     }
-}
+
+    private void setUpVideoView(){
+        // Prepara la URI del vídeo que será reproducido.
+       // String uriPath = "android.resource://" + getPackageName() + "/" + R.raw.video_example;
+        //Uri uri = Uri.parse(uriPath);
+    }
+
+    }
