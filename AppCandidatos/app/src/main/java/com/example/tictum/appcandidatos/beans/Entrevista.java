22586f5c16f2adde8f3c8a12e7a5d12908fa1359
carmenicuadrado <carmenicuadrado@gmail.com>
2017-03-08 11:14:25 +0100

    cambio beans

--- AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/beans/Entrevista.java
+++ AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/beans/Entrevista.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 public class Entrevista {
 
     private int idEntrevista;
+    private String nombreEntrevista;
+    private String nombrePuesto;
     private boolean tieneVideoIntro;
     private ArrayList<Formulario> formularios;
     private ArrayList<Video> videoTransicion;
@@ -92,5 +94,21 @@ public class Entrevista {
 
     public void setMensaje(String mensaje) {
         this.mensaje = mensaje;
+    }
+
+    public String getNombreEntrevista() {
+        return nombreEntrevista;
+    }
+
+    public void setNombreEntrevista(String nombreEntrevista) {
+        this.nombreEntrevista = nombreEntrevista;
+    }
+
+    public String getNombrePuesto() {
+        return nombrePuesto;
+    }
+
+    public void setNombrePuesto(String nombrePuesto) {
+        this.nombrePuesto = nombrePuesto;
     }
 }
