98d3a7ba07fdb2d613e484c7b957d6656d1cb16b
Martinez17 <bryanmartinezh17@gmail.com>
2017-03-09 13:14:59 +0100

    renombrada clase puntuacionentrevista porque tenia una c en medio

--- AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/adapter/AdaptadorPuntuacionEntrevista.java
+++ AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/adapter/AdaptadorPuntuacionEntrevista.java
@@ -9,9 +9,9 @@ import com.example.tictum.appcandidatos.beans.PuntuacionEntrevista;
  * Created by Juanjo on 08/03/2017.
  */
 
-public class AdaptadorPuntuciacionEntrevista extends ArrayAdapter<PuntuacionEntrevista> {
+public class AdaptadorPuntuacionEntrevista extends ArrayAdapter<PuntuacionEntrevista> {
 
-    public AdaptadorPuntuciacionEntrevista(Context context, int resource) {
+    public AdaptadorPuntuacionEntrevista(Context context, int resource) {
         super(context, resource);
 
 
