d9d62bbd89afb08f1b402e0816b2c414e5e37b44
carmenicuadrado <carmenicuadrado@gmail.com>
2017-03-16 11:56:24 +0100

    nuevas clases de negocio

---
+++ AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/business/ArchivoManager.java
@@ -0,0 +1,11 @@
+package com.example.tictum.appcandidatos.business;
+
+import com.example.tictum.appcandidatos.sqliteDAO.ArchivoDAO;
+
+/**
+ * Created by Usuario on 16/03/2017.
+ */
+
+public class ArchivoManager {
+    private ArchivoDAO archivoDAO;
+}
---
+++ AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/business/PreguntaManager.java
@@ -0,0 +1,8 @@
+package com.example.tictum.appcandidatos.business;
+
+/**
+ * Created by Usuario on 16/03/2017.
+ */
+
+public class PreguntaManager {
+}
