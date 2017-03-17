fa2d673ee466e2bfd8bd01c3845a79c0a4b56940
Martinez17 <bryanmartinezh17@gmail.com>
2017-03-17 12:58:06 +0100

    pruebas

--- AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/activities/activity_VideoIntroTransicion.java
+++ AppCandidatos/app/src/main/java/com/example/tictum/appcandidatos/activities/activity_VideoIntroTransicion.java
@@ -1,11 +1,9 @@
 package com.example.tictum.appcandidatos.activities;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.MediaController;
@@ -13,6 +11,7 @@ import android.widget.VideoView;
 
 import com.example.tictum.appcandidatos.R;
 import com.example.tictum.appcandidatos.beans.Entrevista;
+import com.example.tictum.appcandidatos.beans.Video;
 
 public class activity_VideoIntroTransicion extends AppCompatActivity{
 
@@ -23,34 +22,40 @@ public class activity_VideoIntroTransicion extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.layout__video_intro_transicion);
 
         Entrevista entrevista = (Entrevista)getIntent().getSerializableExtra("entrevista");
 
-        // donde vamos a reproducir el video
-        videoView = (VideoView) findViewById(R.id.videoView);
+        if (entrevista.TieneVideoIntro()) {
+            // obtenemos el primer video que es el de transicion para todas las entrevistas si lo tiene
+            Video videoIntro = entrevista.getListaVideos().get(0);
 
-        // ruta del archivo que vamos a reproducir
-        String path = "android.resource://"+getPackageName()+"/"+R.raw.prueba_vertical;
+            // donde vamos a reproducir el video
+            videoView = (VideoView) findViewById(R.id.videoView);
 
-        // convertimos la ruta a una uri para la reproduccion
-        Uri pathUri = Uri.parse(path);
+            // ruta del archivo que vamos a reproducir que sera la ruta del servidor
+            String path = videoIntro.getLinkVideo();
 
-        MediaController mediaController = new MediaController(this);
+            // convertimos la ruta a una uri para la reproduccion
+            Uri pathUri = Uri.parse(path);
 
-        videoView.setMediaController(mediaController);
+            MediaController mediaController = new MediaController(this);
 
-        // añadimos el path
-        videoView.setVideoPath(path);
+            videoView.setMediaController(mediaController);
 
-        // reproducimos el video
-        videoView.start();
+            // añadimos el path
+            videoView.setVideoPath(path);
 
-        // si posicion actual del video (tpo en seg) coincide con la duracion final del video llamamos a la actividad
-       if(videoView.getCurrentPosition() == videoView.getDuration()){
-        // Despues de verse el video completo empezamos la actividad del formulario
-        Intent intent = new Intent(activity_VideoIntroTransicion.this,Activity_PreguntaText.class);
-        startActivity(intent);
-       }
+            // reproducimos el video
+            videoView.start();
+
+            // si posicion actual del video (tpo en seg) coincide con la duracion final del video llamamos a la actividad
+            if (videoView.getCurrentPosition() == videoView.getDuration()) {
+                // Despues de verse el video completo empezamos la actividad del formulario
+                Intent intent = new Intent(activity_VideoIntroTransicion.this, Activity_PreguntaText.class);
+                startActivity(intent);
+            }
+        }
     }
 }
